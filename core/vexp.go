d3047b5f59b21c09825357aa22c466df8365f196
DHL <dhl1402@gmail.com>
2020-04-14 10:55:55 +0700

    Update prev commit

--- core/vexp.go
+++ core/vexp.go
@@ -12,11 +12,19 @@ type VariableExpression struct {
 
 func (e VariableExpression) Evaluate(ec ExecutionContext) (Expression, error) {
 	if exp, ok := ec.Get(e.Name); ok {
-		return exp, nil
+		return exp.Evaluate(ec)
 	}
 	return nil, fmt.Errorf("%s is not defined. [%d,%d]", e.Name, e.Line, e.CharAt)
 }
 
 func (e VariableExpression) GetCharAt() int {
 	return e.CharAt
+}
+
+func (e VariableExpression) GetLine() int {
+	return e.Line
+}
+
+func (e VariableExpression) GetType() string {
+	return "variable"
 }
